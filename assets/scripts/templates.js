83325c9a35356d6b35119fda1fd2f8d22336bfe9
Blubbi33 <96566529+Blubbi33@users.noreply.github.com>
2025-09-04 01:49:21 +0200

    added HTMLTemplates

---
+++ assets/scripts/templates.js
@@ -0,0 +1,136 @@
+function pokeCardHTMLTemplate(p) {
+    return `<div class="pokecard" onclick="toggleModal(${p.id})">
+                <div class="flip-card-inner" id="flipcard-${p.id}">
+                    <div class="pokecard flip-card-front" id="innerpokecard-${p.id}">
+                         <h3 id="pokename">${p.name}</h3>
+                         <img id="pokepic" src="${p.sprites.other.dream_world.front_default}"alt="">   
+                         <div id="poketype-${p.id}" class="poketype">           
+                         </div>        
+                    </div>
+                </div>     
+            </div>             
+             `
+
+}
+
+function pokeCardTypeHTMLTemplate(t) {
+    return `<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-vii/lets-go-pikachu-lets-go-eevee/${t}.png"alt="Type">
+                 `
+}
+
+function pokeStatsHTMLTemplate(s) {
+    return ` <div class="pokecard flip-card-back">
+                <div class="pokestats">
+                    <h3 id="pokename">${s.name.toUpperCase()}</h3>
+                    <div class="stat">
+                        <label for="hp">HP</label>
+                        <p>${s.stats[0].base_stat}</p>
+                    </div>
+                    <progress value="${s.stats[0].base_stat}" max="255" id="hp">HP</progress>
+                    <div class="stat">
+                        <label for="atk">ATK</label>
+                        <p>${s.stats[1].base_stat}</p>
+                    </div>
+                    <progress value="${s.stats[1].base_stat}" max="255" id="atk">49</progress>
+                    <div class="stat">
+                        <label for="def">DEF</label>
+                        <p>${s.stats[2].base_stat}</p>
+                    </div>
+                    <progress value="${s.stats[2].base_stat}" max="255" id="def">49</progress>
+                    <div class="stat">
+                        <label for="satk">S-ATK</label>
+                        <p>${s.stats[3].base_stat}</p>
+                    </div>
+                    <progress value="${s.stats[3].base_stat}" max="255" id="satk">65</progress>
+                    <div class="stat">
+                        <label for="sdef">S-DEF</label>
+                        <p>${s.stats[4].base_stat}</p>
+                    </div>
+                    <progress value="${s.stats[4].base_stat}" max="255" id="sdef">65</progress>
+                    <div class="stat">
+                        <label for="spd">SPD</label>
+                        <p>${s.stats[5].base_stat}</p>
+                    </div>
+                    <progress value="${s.stats[5].base_stat}" max="255" id="spd">45</progress>
+                </div> 
+            </div> `
+}
+
+function pokeGenInfoHTMLTemplate(p) {
+    return `
+        <div class="deep-info" id="deep-info">    
+            <div>
+                <img src="${p.sprites.other.dream_world.front_default}"alt="">
+            </div>
+            <div class="geninfo">    
+                <h3>Height: <br>${p.height * 10}cm</h3>
+                <h3>Weight: <br>${p.weight * 10}g</h3>
+                <h3>BaseXP: <br>${p.base_experience}</h3>
+            </div> 
+        </div>`
+}
+
+function pokeEvoChainHTMLTemplate(p) {
+    return `
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${p.id}.svg"
+                alt="evo1">
+    `
+}
+
+
+function overlayHeaderHTMLTemplate(p) {
+    return `
+    <div class="overlay-header" id="overlay-header" onclick="preventBubbling(event)">
+        <button>←</button>
+        <h3>${p.name}</h3>
+        <button>→</button>
+    </div>
+    <div class="tabs" onclick="preventBubbling(event)">
+        <button onclick="toggleGenInfoTab(${p.id})">
+            <h4 class="active" id="tab-geninfo">Gen. info</h4>
+        </button>
+        <button onclick="toggleStatsTab(${p.id})">
+            <h4 class="" id="tab-stats">Stats</h4>
+        </button>
+        <button onclick="toggleEvoTab(${p.id})">
+            <h4 class="" id="tab-evochain">Evo Chain</h4>
+        </button>
+    </div>
+    `
+}
+
+function pokeOverlayStatsHTMLTemplate(s) {
+    return `    <div class="pokestats">
+                    <div class="stat">
+                        <label for="hp">HP</label>
+                        <p>${s.stats[0].base_stat}</p>
+                    </div>
+                    <progress value="${s.stats[0].base_stat}" max="255" id="hp">HP</progress>
+                    <div class="stat">
+                        <label for="atk">ATK</label>
+                        <p>${s.stats[1].base_stat}</p>
+                    </div>
+                    <progress value="${s.stats[1].base_stat}" max="255" id="atk">49</progress>
+                    <div class="stat">
+                        <label for="def">DEF</label>
+                        <p>${s.stats[2].base_stat}</p>
+                    </div>
+                    <progress value="${s.stats[2].base_stat}" max="255" id="def">49</progress>
+                    <div class="stat">
+                        <label for="satk">S-ATK</label>
+                        <p>${s.stats[3].base_stat}</p>
+                    </div>
+                    <progress value="${s.stats[3].base_stat}" max="255" id="satk">65</progress>
+                    <div class="stat">
+                        <label for="sdef">S-DEF</label>
+                        <p>${s.stats[4].base_stat}</p>
+                    </div>
+                    <progress value="${s.stats[4].base_stat}" max="255" id="sdef">65</progress>
+                    <div class="stat">
+                        <label for="spd">SPD</label>
+                        <p>${s.stats[5].base_stat}</p>
+                    </div>
+                    <progress value="${s.stats[5].base_stat}" max="255" id="spd">45</progress>
+                </div>
+    `
+}
